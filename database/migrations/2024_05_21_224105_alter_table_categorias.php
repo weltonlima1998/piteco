cca3cb8ebf5de5f4c0f8c75d3441b3f0b1221055
Welton Peppes <weltonlima1998@gmail.com>
2024-05-27 01:04:46 -0300

    atualização login, categoria e add

---
+++ database/migrations/2024_05_21_224105_alter_table_categorias.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categorias', function (Blueprint $table) {
+            $table->dropColumn('descricao');
+
+            $table->boolean('trans')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categorias', function (Blueprint $table) {
+            $table->string('descricao');
+
+            $table->dropColumn('trans');
+        });
+    }
+};
